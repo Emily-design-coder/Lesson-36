ed00cefdb78e12958e10ce76f0c5c23119f31deb
Emily-design-coder <emilyrobb2012@icloud.com>
2026-03-08 15:18:21 +1100

    initial commit

---
+++ activity3.py
@@ -0,0 +1,15 @@
+first_name= "Codingal"
+last_name= "Educations"
+full_name= first_name+last_name
+example= "Haa"*5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String multipled 5 times gives this result :", example)
+
+word= 'coding'
+print("Lenght of string :", len(word))
+print("First letter of string :", word[0])
+print("Last letter of string :", word[-1])
+print("String sliced :", word[0:3])
