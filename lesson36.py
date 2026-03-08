a60808e01bf48fad75efa3675470c3c8452bb28d
Emily-design-coder <emilyrobb2012@icloud.com>
2026-03-08 14:50:39 +1100

    initial commit

---
+++ lesson36.py
@@ -0,0 +1,16 @@
+name="Emily"
+age=14
+num=18.47
+value=True
+print("Before Typecasting: ")
+print(name,type(name))
+print(age,type(age))
+print(num,type(num))
+print(value,type(value))
+print("After Typecasting: ")
+age1= float(age)
+print(age1,type(age1))
+num_1=int(num)
+print(num_1,type(num_1))
+value1=str(value)
+print(value1,type(value1))
