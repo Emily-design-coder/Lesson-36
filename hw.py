996dd0fd70e879fbd10eb978d47a454e8b383cd1
Emily-design-coder <emilyrobb2012@icloud.com>
2026-03-11 20:00:46 +1100

    initial commit

---
+++ hw.py
@@ -0,0 +1,10 @@
+x = input("Enter value of x: ")
+y = input("Enter value of y: ")
+z = input("Enter value of z: ")
+
+x, z, y = z, y, x
+
+print("After swapping:")
+print("x =", x)
+print("y =", y)
+print("z =", z)
